621a50c0b7fdd1f8dfa694d35eb2bbfbcfdc425f
lightsblocker <lightsblocker@gmail.com>
2026-04-14 14:21:28 -0300

    Create feedback.js

---
+++ assets/pages/feedback/feedback.js
@@ -0,0 +1,10 @@
+#feedback-screen form {
+    display: flex;
+    flex-direction: column;
+}
+
+#feedback-screen input[readonly] {
+    background-color: #2a2a2a; /* Mais escuro para indicar que não edita */
+    color: #888;
+    cursor: not-allowed;
+}
